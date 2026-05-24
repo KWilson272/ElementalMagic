e43fb24f8444948e68a0cd3d97b071befb3a8066
kwilson272 <kaleb.wilson272@gmail.com>
2026-05-23 22:32:26 -0700

    Add Ray Component

---
+++ core/src/main/java/me/kwilson272/elementalmagic/core/gameplay/components/Ray.java
@@ -0,0 +1,62 @@
+package me.kwilson272.elementalmagic.core.gameplay.components;
+
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.util.Vector;
+
+import me.kwilson272.elementalmagic.api.util.BlockUtil;
+
+public abstract class Ray {
+
+    private Location location;
+    private double speed;
+    private double range;
+    private double iterSpeed;
+
+    public Ray(Location location, double speed, double range) {
+        this.location = location;
+        this.speed = speed;
+        this.range = range;
+        this.iterSpeed = calculateIterSpeed();
+    }
+
+    // Should help keep visuals decently similar across speeds
+    private double calculateIterSpeed() {
+        if (speed <= 1.0) {
+            return speed;
+        } else {
+            int toRound = ((int) speed) + 5;
+            double tenMod = Math.round(toRound / 10.0);
+            return speed / (Math.floor(speed) + tenMod + 1);
+        }
+    }
+
+    public boolean progress() {
+        for (double i = 0; i < speed; i += iterSpeed) {
+            Vector dir = getDirection().multiply(iterSpeed);
+            Location prev = location.clone();
+            location.add(dir);
+
+            if (collides(location.getBlock()) 
+                    || BlockUtil.collidesDiagonally(prev, location, this::collides)) {
+                return false; 
+            }
+
+            if (!moveTo(location)) {
+                return false;
+            }
+
+            range -= iterSpeed;
+            if (range <= 0) {
+                return false;
+            }
+        }
+        return range > 0;
+    }
+    
+    public abstract boolean collides(Block block);
+
+    public abstract boolean moveTo(Location loc);
+
+    public abstract Vector getDirection();
+}
